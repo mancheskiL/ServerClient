16fbf1a2fa58f2c8fe6ce0fa06124496d35201ee
MancheskiL <lucas.mancheskicol@gmail.com>
2020-08-30 14:52:22 +0200

    create account page detects submit click and pulls input string

--- scripts/create_account_submit.js
+++ scripts/create_account_submit.js
@@ -6,22 +6,12 @@ function debug_send(message){
   ipcRenderer.send('debug-message', output)
 }
 
-function print_test(){
-  document.getElementById('demo').innerHTML = 'test'
-}
-
 const button = document.getElementById('submit-account')
-const testButton = document.getElementById('test')
 
-
-button.addEventListener('click', () => {
-  document.getElementById('demo').innerHTML = 'test'
-  console.log("test")
+button.addEventListener('click', (event) => {
+  console.log("submit button was pressed")
   // debug_send('test')
-  let output = 'test'
-  ipcRenderer.send('debug-message', output)
-})
-
-testButton.addEventListener('click', () => {
-  ipcRenderer.invoke('debug-message', 'test')
+  let output = document.getElementById("username-input").value
+  console.log(output)
+  ipcRenderer.invoke('debug-message', output)
 })
