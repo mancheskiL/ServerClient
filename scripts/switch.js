fe136c640b2e217799e13db23974c8bae0b14f86
MancheskiL <lucas.mancheskicol@gmail.com>
2020-08-30 13:48:16 +0200

    can switch pages with JS

--- scripts/switch.js
+++ scripts/switch.js
@@ -1,15 +1,16 @@
 const electron = require('electron')
 const { ipcRenderer } = require('electron')
 
-function change_page(fileName){
-  let end = '.html'
-  let output = fileName + end
-  ipcRenderer.invoke('perform-action', output)
-}
-
+// adds 'click' event listener to given object
+// event communicates to main.js to relevant page
 function addListener(item){
   item.addEventListener('click', (event) => {
-    console.log(item.innerHTML)
+    // console.log(item.innerHTML)
+    // console.log(item.id)
+    let beginning = item.id
+    let end = '.html'
+    let output = beginning + end
+    ipcRenderer.invoke('perform-action', output)
   })
 }
 
