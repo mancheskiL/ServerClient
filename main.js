67ca33d665c73822d7ae8510894c0bb419844944
Lucas <lucas.mancheskicol@gmail.com>
2020-09-01 21:15:29 +0200

    db work

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ ipcMain.handle('create-account', (event, message) => {
   console.log(message)
   console.log('attempting to contact mongodb')
 
-  const MongoClient = require('mongodb').MongoClients;
+  const MongoClient = require('mongodb').MongoClient;
   const uri = "mongodb+srv://luman_og:12213119@lumandb.z488z.gcp.mongodb.net/Pi_Serv?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
   client.connect(err => {
