2b86b32151b2640156ca97325db44c163b77ac21
MancheskiL <lucas.mancheskicol@gmail.com>
2020-09-02 06:41:39 +0200

    can successfully connect to atlas through render process

--- scripts/create_account_submit.js
+++ scripts/create_account_submit.js
@@ -1,24 +1,25 @@
 const { ipcRenderer } = require('electron')
-
-// function debug_send(message){
-//   console.log('sending to main')
-//   let output = message
-//   ipcRenderer.send('debug-message', output)
-// }
-//
-// function create_account(username){
-//   console.log('sending username to main for processing')
-//   let output = username
-//   ipcRenderer.invoke('create-account', output)
-// }
+const MongoClient = require('mongodb').MongoClient;
 
 const button = document.getElementById('submit-account')
 
+// listen for submit button click
 button.addEventListener('click', (event) => {
   console.log("submit button was pressed")
   // debug_send('test')
   let output = document.getElementById("username-input").value
   console.log(output)
-  ipcRenderer.invoke('create-account', output)
-  location.reload()
+  // ipcRenderer.invoke('create-account', output)
+
+  // const uri = "mongodb+srv://luman_og:12213119@lumandb.z488z.gcp.mongodb.net/Pi_Serv?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://luman_og:12213119@lumandb.z488z.gcp.mongodb.net/sample_airbnb?ssl=true&authSource=admin&retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  // const client = new MongoClient(uri);
+  client.connect(err => {
+    if (err) throw err;
+    console.log('connected to db!');
+    client.close();
+  })
+
+  // location.reload()
 })
