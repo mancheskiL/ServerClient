a510d8892e2ed785e48beeb38a4e85555704799a
MancheskiL <lucas.mancheskicol@gmail.com>
2020-09-03 06:04:24 +0200

    can connect to and query DB. DevTools will turn on automatically for development

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function createWindow () {
   // win.loadFile('public/login.html')
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
--- scripts/create_account_submit.js
+++ scripts/create_account_submit.js
@@ -8,21 +8,31 @@ const button = document.getElementById('submit-account')
 button.addEventListener('click', (event) => {
   console.log("submit button was pressed")
   // debug_send('test')
-  let output = document.getElementById("username-input").value
-  console.log(output)
+  let input_name = document.getElementById("username-input").value
+  console.log(input_name)
   // ipcRenderer.invoke('create-account', output)
 
-  const uri = "mongodb+srv://" + process.env.USER + ":" + process.env.PASSWORD + "@" process.env.SITE + ".z488z.gcp.mongodb.net/Pi_Serv?retryWrites=true&w=majority";
-  // const uri = "mongodb+srv://" +
-  //   process.env.USER + ":" + process.env.PASSWORD + "@" + process.env.SITE +
-  //   ".z488z.gcp.mongodb.net/Pi_Serv?ssl=true&authSource=admin&retryWrites=true&w=majority";
+  const uri = "mongodb+srv://" +
+    process.env.USER + ":" + process.env.PASSWORD + "@" + process.env.SITE +
+    ".z488z.gcp.mongodb.net/Pi_Serv?ssl=true&retryWrites=true&w=majority"
 
   const client = new MongoClient(uri, { useNewUrlParser: true })
   // const client = new MongoClient(uri);
-  client.connect(err => {
+  client.connect((err, db) => {
     if (err) throw err
     console.log('connected to db!')
-    client.close()
+    var dbo = db.db('Pi_Serv')
+    var query = { account: input_name}
+    dbo.collection('accounts').find(query).toArray((err, result) => {
+      if (err) throw err
+      if (result.length > 0) {
+        console.log('username already exists')
+      } else {
+        console.log('creating new user account')
+      }
+      console.log(result)
+      db.close()
+    })
   })
 
   // location.reload()
