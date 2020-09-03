c405616c4f9dd2724e3d2286240324297c7cd6d5
Lucas <lucas.mancheskicol@gmail.com>
2020-09-03 21:53:44 +0200

    comment documentation and some desirable TODO's for when I have time

--- scripts/create_account_submit.js
+++ scripts/create_account_submit.js
@@ -4,31 +4,36 @@ require('dotenv').config()
 
 const button = document.getElementById('submit-account')
 
-console.log(process.env.USER)
-console.log(process.env.PASSWORD)
-console.log(process.env.SITE)
+// for checking proper .env variables if debugging
+// console.log(process.env.USER)
+// console.log(process.env.PASSWORD)
+// console.log(process.env.SITE)
 
 // listen for submit button click
 button.addEventListener('click', (event) => {
   console.log("submit button was pressed")
-  // debug_send('test')
+  // get String text from input box
   let input_name = document.getElementById("username-input").value
   console.log(input_name)
-  // ipcRenderer.invoke('create-account', output)
 
+  // url for connection to server
+  // gets credentials from env
   const uri = "mongodb+srv://" +
     process.env.USER + ":" + process.env.PASSWORD + "@" + process.env.SITE +
     ".z488z.gcp.mongodb.net/Pi_Serv?ssl=true&retryWrites=true&w=majority"
 
+  // connect to cloud DB
   const client = new MongoClient(uri, { useNewUrlParser: true })
-  // const client = new MongoClient(uri);
   client.connect((err, db) => {
     if (err) throw err
     console.log('connected to db!')
+    // getting DB object
     var dbo = db.db('Pi_Serv')
     var query = { account: input_name}
+    // query collection from DB with predefined query object
     dbo.collection('accounts').find(query).toArray((err, result) => {
       if (err) throw err
+      // length > 0 means account already exists, otherwise make account
       if (result.length > 0) {
         console.log('username already exists')
       } else {
@@ -42,6 +47,12 @@ button.addEventListener('click', (event) => {
       db.close()
     })
   })
+  // refresh page so input line blanks after submission
+  location.reload()
 
-  // location.reload()
+  // TODO: make input line blank using JS after submit click
+  // instead of reloading the entire page and losing console.log info
+
+  // TODO: make DB response text area for displaying status of DB
+  // communications. Submit click can reset the text area
 })
