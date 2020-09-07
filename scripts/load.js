a55d43da18cc1fff773fa7f87af1dd3b36418192
Lucas <lucas.mancheskicol@gmail.com>
2020-09-07 22:08:23 +0200

    building out load JS. can get response from webserver confirming web server is alive

--- scripts/load.js
+++ scripts/load.js
@@ -1,4 +1,17 @@
+var check = 0
 // TODO: need to check status of Pi node.js server instance
+var xhttp = new XMLHttpRequest()
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    check = check + 1
+    console.log(check)
+  }
+}
+
+xhttp.open("GET", "http://192.168.178.43:8080", true)
+xhttp.send()
+
+
 // TODO: check for persistent user cookie type file
 // if file exists, skip login screen
 // only main process can handle cookies
